2e78479aa1c7dae2bd3e0ce85b3d1542a89e2f01
tomee <tomee6@gmail.com>
2014-04-09 17:00:28 +0200

    code reformat

--- jslay-0.4.2.js
+++ jslay-0.4.2.js
@@ -1,20 +1,20 @@
 var jslay = {};
-(function() {
+(function () {
     var rules = [];
     var rulesDirty = false;
     var constants = {};
 
     var elements = [];
 
-    jslay.setConstant = function(name, value) {
+    jslay.setConstant = function (name, value) {
         constants[name] = value;
     };
 
-    jslay.clearConstants = function() {
+    jslay.clearConstants = function () {
         constants = {};
     };
 
-    jslay.setRule = function(element, left, top, width, height) {
+    jslay.setRule = function (element, left, top, width, height) {
         deleteExistingRule(element, 'left');
         deleteExistingRule(element, 'top');
         deleteExistingRule(element, 'width');
@@ -27,19 +27,19 @@ var jslay = {};
         rulesDirty = true;
     };
 
-    jslay.clearRules = function() {
+    jslay.clearRules = function () {
         rules = [];
     };
 
-    jslay.clear = function() {
+    jslay.clear = function () {
         rules = [];
         constants = {};
     };
 
     function findExistingRule(element, property) {
-        for(var i = 0; i < rules.length; i++) {
+        for (var i = 0; i < rules.length; i++) {
             var rule = rules[i];
-            if(rule.element == element && rule.property == property) {
+            if (rule.element == element && rule.property == property) {
                 return rule;
             }
         }
@@ -48,9 +48,9 @@ var jslay = {};
 
     function deleteExistingRule(element, property) {
         var result = null;
-        for(var i = 0; i < rules.length; i++) {
+        for (var i = 0; i < rules.length; i++) {
             var rule = rules[i];
-            if(rule.element == element && rule.property == property) {
+            if (rule.element == element && rule.property == property) {
                 result = rule;
                 rules.splice(i, 1);
                 i--;
@@ -79,21 +79,21 @@ var jslay = {};
 
     function lex(expression) {
         var result = [];
-        if(expression) {
-            while(expression.length > 0) {
+        if (expression) {
+            while (expression.length > 0) {
                 var found = false;
-                for(var i = 0; i < tokenDefinitions.length; i++) {
+                for (var i = 0; i < tokenDefinitions.length; i++) {
                     var tokenDefinition = tokenDefinitions[i];
                     var type = tokenDefinition[0];
                     var regexp = tokenDefinition[1];
                     var match = regexp.exec(expression);
-                    if(match && match.length > 0 && match.index == 0) {
+                    if (match && match.length > 0 && match.index == 0) {
                         var matchedString = match[0];
-                        if(type != 'whitespace') {
+                        if (type != 'whitespace') {
                             var value;
-                            if(type == 'number') {
+                            if (type == 'number') {
                                 value = Number(matchedString);
-                            } else if(type == 'element') {
+                            } else if (type == 'element') {
                                 value = matchedString.substr(1);
                             } else {
                                 value = matchedString;
@@ -105,7 +105,7 @@ var jslay = {};
                         break;
                     }
                 }
-                if(!found) {
+                if (!found) {
                     return null;
                 }
             }
@@ -122,22 +122,22 @@ var jslay = {};
     function compile(tokens) {
         var expressionStack = [];
         var operatorStack = [];
-        for(var i = 0; i < tokens.length; i++) {
+        for (var i = 0; i < tokens.length; i++) {
             var token = tokens[i];
-            if(token[0] == 'operator') {
+            if (token[0] == 'operator') {
                 var precedence = findPrecedence(token[1]);
-                while(operatorStack.length > 0 && operatorStack[operatorStack.length - 1][0] <= precedence) {
+                while (operatorStack.length > 0 && operatorStack[operatorStack.length - 1][0] <= precedence) {
                     createSubExpression();
                 }
                 operatorStack.push([ precedence, token[1] ]);
-            } else if(token[0] == 'parenthesis') {
-                if(token[1] == '(') {
+            } else if (token[0] == 'parenthesis') {
+                if (token[1] == '(') {
                     operatorStack.push([ operators.length, token[1] ]);
-                } else if(token[1] == ')') {
-                    while(operatorStack.length > 0 && operatorStack[operatorStack.length - 1][1] != '(') {
+                } else if (token[1] == ')') {
+                    while (operatorStack.length > 0 && operatorStack[operatorStack.length - 1][1] != '(') {
                         createSubExpression();
                     }
-                    if(operatorStack.length > 0) {
+                    if (operatorStack.length > 0) {
                         operatorStack.pop();
                     }
                 }
@@ -145,7 +145,7 @@ var jslay = {};
                 expressionStack.push([ 'operand', token ]);
             }
         }
-        while(operatorStack.length > 0) {
+        while (operatorStack.length > 0) {
             createSubExpression();
         }
         return expressionStack.pop();
@@ -154,7 +154,7 @@ var jslay = {};
             var operator = operatorStack.pop();
             var right = expressionStack.pop();
             var left = expressionStack.pop();
-            if(operator[1] == '.') {
+            if (operator[1] == '.') {
                 right[1][0] = 'property';
             }
             var subExpression = [ 'expression', left, operator[1], right ];
@@ -163,8 +163,8 @@ var jslay = {};
     }
 
     function findPrecedence(operator) {
-        for(var i = 0; i < operators.length; i++) {
-            if(operators[i].indexOf(operator) != -1) {
+        for (var i = 0; i < operators.length; i++) {
+            if (operators[i].indexOf(operator) != -1) {
                 return i;
             }
         }
@@ -172,12 +172,12 @@ var jslay = {};
     }
 
     function buildRules() {
-        if(rulesDirty) {
+        if (rulesDirty) {
             rulesDirty = false;
 
-            for(var i = 0; i < rules.length; i++) {
+            for (var i = 0; i < rules.length; i++) {
                 var element = rules[i].element;
-                if(elements.indexOf(element) == -1) {
+                if (elements.indexOf(element) == -1) {
                     elements.push(element);
                 }
                 rules[i].dependencies = [];
@@ -190,22 +190,22 @@ var jslay = {};
     }
 
     function findDependencies(rootRule, expression) {
-        if(!expression) return;
-        if(expression[0] == 'expression') {
-            if(expression[2] == '.') {
+        if (!expression) return;
+        if (expression[0] == 'expression') {
+            if (expression[2] == '.') {
                 var left = expression[1][1][1];
                 var right = expression[3][1][1];
                 var properties;
-                if(right == 'bottom') {
+                if (right == 'bottom') {
                     properties = [ 'top', 'height' ];
-                } else if(right == 'right') {
+                } else if (right == 'right') {
                     properties = [ 'left', 'width' ];
                 } else {
                     properties = [ right ];
                 }
-                for(var i = 0; i < properties.length; i++) {
+                for (var i = 0; i < properties.length; i++) {
                     var rule = findExistingRule(left, properties[i]);
-                    if(rule) {
+                    if (rule) {
                         rootRule.dependencies.push(rule);
                     }
                 }
@@ -218,11 +218,11 @@ var jslay = {};
 
     function removeDuplicates(array) {
         var result = [];
-        for(var i = 0; i < array.length; i++) {
+        for (var i = 0; i < array.length; i++) {
             var element = array[i];
             var exists = false;
-            for(var j = 0; j < result.length; j++) {
-                if(result[j] == element) {
+            for (var j = 0; j < result.length; j++) {
+                if (result[j] == element) {
                     exists = true;
                     break;
                 }
@@ -234,23 +234,23 @@ var jslay = {};
 
     function sortElementLayoutOrder() {
         var reordered = [];
-        for(var i = 0; i < rules.length; i++) {
+        for (var i = 0; i < rules.length; i++) {
             var rule = rules[i];
-            if(reordered.indexOf(rule) == -1) {
+            if (reordered.indexOf(rule) == -1) {
                 addInOrder(rule, []);
             }
         }
         rules = reordered;
 
         function addInOrder(rule, elementStack) {
-            for(var j = 0; j < elementStack.length; j++) {
-                if(rule.element == elementStack[j][0] && rule.property == elementStack[j][1]) {
+            for (var j = 0; j < elementStack.length; j++) {
+                if (rule.element == elementStack[j][0] && rule.property == elementStack[j][1]) {
                     return;
                 }
             }
-            if(rule.dependencies.length > 0) {
+            if (rule.dependencies.length > 0) {
                 elementStack.push([rule.element, rule.property]);
-                for(j = 0; j < rule.dependencies.length; j++) {
+                for (j = 0; j < rule.dependencies.length; j++) {
                     addInOrder(rule.dependencies[j], elementStack);
                 }
                 elementStack.pop();
@@ -259,15 +259,15 @@ var jslay = {};
         }
     }
 
-    jslay.layout = function() {
+    jslay.layout = function () {
         var elementPositions = jslay.getLayout();
         jslay.applyLayout(elementPositions);
     };
 
-    jslay.getLayout = function() {
+    jslay.getLayout = function () {
         buildRules();
         var elementPositions = {};
-        for(var i = 0; i < elements.length; i++) {
+        for (var i = 0; i < elements.length; i++) {
             var elementName = elements[i];
             var element = document.getElementById(elementName);
             elementPositions[elementName] = {
@@ -277,17 +277,17 @@ var jslay = {};
                 height: element.offsetHeight
             };
         }
-        for(i = 0; i < rules.length; i++) {
+        for (i = 0; i < rules.length; i++) {
             var rule = rules[i];
             elementName = rule.element;
-            if(rule.rule) {
+            if (rule.rule) {
                 elementPositions[elementName][rule.property] = run(rule.rule, elementPositions);
             } else {
                 element = document.getElementById(elementName);
-                if(rule.property == 'width') {
+                if (rule.property == 'width') {
                     elementPositions[elementName]['width'] = element.offsetWidth;
                     elementPositions[elementName]['selfWidth'] = true;
-                } else if(rule.property == 'height') {
+                } else if (rule.property == 'height') {
                     elementPositions[elementName]['height'] = element.offsetHeight;
                     elementPositions[elementName]['selfHeight'] = true;
                 }
@@ -296,32 +296,32 @@ var jslay = {};
         return elementPositions;
     };
 
-    jslay.applyLayout = function(elementPositions) {
-        for(var i = 0; i < elements.length; i++) {
+    jslay.applyLayout = function (elementPositions) {
+        for (var i = 0; i < elements.length; i++) {
             var elementName = elements[i];
             var element = document.getElementById(elementName);
             var elementPosition = elementPositions[elementName];
             element.style.left = elementPosition.left + 'px';
             element.style.top = elementPosition.top + 'px';
-            if(!elementPositions[elementName]['selfWidth']) {
+            if (!elementPositions[elementName]['selfWidth']) {
                 element.style.width = elementPosition.width + 'px';
             }
-            if(!elementPositions[elementName]['selfHeight']) {
+            if (!elementPositions[elementName]['selfHeight']) {
                 element.style.height = elementPosition.height + 'px';
             }
         }
     };
 
     function run(expression, elementPositions) {
-        if(!expression) return;
+        if (!expression) return;
         var expressionType = expression[0];
-        if(expressionType == 'expression') {
+        if (expressionType == 'expression') {
             var left = run(expression[1], elementPositions);
             var operator = expression[2];
             var right = run(expression[3], elementPositions);
-            if(operator == '.') {
+            if (operator == '.') {
                 var elementPosition = elementPositions[left];
-                if(elementPosition == null) {
+                if (elementPosition == null) {
                     var element = document.getElementById(left);
                     elementPosition = {
                         left: element.offsetLeft,
@@ -330,27 +330,27 @@ var jslay = {};
                         height: element.offsetHeight
                     };
                 }
-                if(right == 'right') {
+                if (right == 'right') {
                     return elementPosition.left + elementPosition.width;
-                } else if(right == 'bottom') {
+                } else if (right == 'bottom') {
                     return elementPosition.top + elementPosition.height;
                 } else {
                     return elementPosition[right];
                 }
-            } else if(operator == '+') {
+            } else if (operator == '+') {
                 return left + right;
-            } else if(operator == '-') {
+            } else if (operator == '-') {
                 return left - right;
-            } else if(operator == '*') {
+            } else if (operator == '*') {
                 return left * right;
-            } else if(operator == '/') {
+            } else if (operator == '/') {
                 return left / right;
             }
-        } else if(expressionType == 'operand') {
+        } else if (expressionType == 'operand') {
             var token = expression[1];
             var tokenType = token[0];
             var result;
-            if(tokenType == 'name') {
+            if (tokenType == 'name') {
                 result = constants[token[1]];
             } else {
                 result = token[1];
